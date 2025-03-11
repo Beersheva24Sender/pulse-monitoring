f0a9e49d1ec1c45e869f8756bef5a8ade1059956
Daniel Sender <70211437+daniel1sender@users.noreply.github.com>
2025-03-11 15:32:52 +0200

    Implement MiddlewareDataStream interface and DynamoDbStream for sensor data handling

--- pulse-monitoring-api/src/main/java/telran/monitoring/logging/LoggerStandard.java
+++ pulse-monitoring-api/src/main/java/telran/monitoring/logging/LoggerStandard.java
@@ -1,36 +1,50 @@
 package telran.monitoring.logging;
- 
- import java.util.HashMap;
- import java.util.logging.*;
- public class LoggerStandard implements Logger{
-     String defaultValue = "info";
-     java.util.logging.Logger logger; 
-     static HashMap<String, String> levelsMap = new HashMap<>(){{
-         put("debug", "config");
-         put("trace", "fine");
-     }};
-     public LoggerStandard(String loggerName) {
-         logger = LogManager.getLogManager().getLogger(loggerName);
-         String level = System.getenv("LOGGER_LEVEL");
-         String javaLevel = levelsMap.get(level);
-         if(javaLevel != null) {
-             level = javaLevel;
-         }
-         logger.setLevel(Level.parse(level.toUpperCase()));
-     }
-     @Override
-     public void setDefaultLevel(String defaultValue) {
-        String javaLevel = levelsMap.get(defaultValue);
-        this.defaultValue = javaLevel != null ? javaLevel : defaultValue;
-     }
- 
-     @Override
-     public void log(String level, String message) {
-         String javaLevel = levelsMap.get(level);
-         if (javaLevel != null) {
-             level = javaLevel;
-         }
-         logger.log(Level.parse(level.toUpperCase()), message);
-     }
- 
- }
+
+import java.util.HashMap;
+import java.util.logging.*;
+
+public class LoggerStandard implements Logger {
+
+    java.util.logging.Logger logger;
+    static String defaultValue = Logger.defaultValue;
+    static HashMap<String, String> levelsMap = new HashMap<>() {
+        {
+            put("debug", "config");
+            put("trace", "fine");
+            put("error", "severe");
+        }
+    };
+
+    static void setDefaultLevel(String defaultValue) {
+        LoggerStandard.defaultValue = defaultValue;
+    }
+
+    public LoggerStandard(String loggerName) {
+        LogManager.getLogManager().reset();
+        this.logger = java.util.logging.Logger.getLogger(loggerName);
+        String level = System.getenv("LOGGER_LEVEL");
+        if (level == null) {
+            level = defaultValue;
+        }
+        String javaLevel = levelsMap.get(level);
+        if (javaLevel != null) {
+            level = javaLevel;
+        }
+        Level loggerLevel = Level.parse(level.toUpperCase());
+        logger.setLevel(loggerLevel);
+        Handler consoleHandler = new ConsoleHandler();
+        consoleHandler.setLevel(loggerLevel);
+        logger.addHandler(consoleHandler);
+
+    }
+
+    @Override
+    public void log(String level, String message) {
+        String javaLevel = levelsMap.get(level);
+        if (javaLevel != null) {
+            level = javaLevel;
+        }
+        logger.log(Level.parse(level.toUpperCase()), message);
+    }
+
+}
